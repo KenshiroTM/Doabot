672bce5b57df60155e4bb94a80cb1a4983ec7095
Marcin <65854348+KenshiroTM@users.noreply.github.com>
2026-09-09 12:05:23 +0200

    Update misc.py

--- Cogs/misc.py
+++ Cogs/misc.py
@@ -23,13 +23,16 @@ class Misc(commands.Cog, name="misc"):
             await ctx.reply("Bubble ratio has to be between 0.1 or 0.5!")
             return
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get(image_url) as resp:
-                image_data = await resp.read()
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(image_url) as resp:
+                    image_data = await resp.read()
 
-        result = await speechify(image_data, bubble_number, bubble_height_ratio)
-        file = discord.File(result, filename="speechified.gif")
-        await ctx.reply(file=file)
+            result = await speechify(image_data, bubble_number, bubble_height_ratio)
+            file = discord.File(result, filename="speechified.gif")
+            await ctx.reply(file=file)
+        except Exception as e:
+            await ctx.reply("Please enter a valid image URL (copy image link option).")
 
 async def setup(bot):
     await bot.add_cog(Misc(bot))
